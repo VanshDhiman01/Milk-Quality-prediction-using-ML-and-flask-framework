d123b750531293f3c76bf9febc4a9be9d0225eb1
VanshDhiman01 <112922491+VanshDhiman01@users.noreply.github.com>
2024-06-17 10:51:11 +0530

    milk quality prediction

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,17 @@
+import numpy as np
+from flask import Flask, render_template,request
+import pickle#Initialize the flask App
+app = Flask(__name__,template_folder="templates")
+model = pickle.load(open('model_lrr.pkl', 'rb'))
+@app.route('/')
+def home():
+    return render_template('index.html')
+@app.route('/predict',methods=['POST'])
+def predict():
+    #For rendering results on HTML GUI
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    return render_template('index.html', prediction_text='Milk Quality is: :{}'.format(prediction))
+if __name__ == "__main__":
+    app.run(debug=True)
